ff73726d7b3a485a170d568a9aae111a69e6c59f
Evan Shapiro <evan.a.shapiro@gmail.com>
2013-06-24 13:46:13 -0700

    music2: tap album art to toggle info

--- test_apps/music2/js/UI/CurrentMusicPageSource.js
+++ test_apps/music2/js/UI/CurrentMusicPageSource.js
@@ -11,9 +11,28 @@ var CurrentMusicPageSource = function(){
 
   Utils.setupPassEvent(this, 'hideCurrentSourceView');
   Utils.setupPassEvent(this, 'showCurrentSourceView');
+
+  var imgTapManager = new TapManager(this.dom.currentSourceImg);
+
+  imgTapManager.ontap = this.toggleInfoVisibility.bind(this);
 }
 
 CurrentMusicPageSource.prototype = {
+  toggleInfoVisibility: function(){
+    if (!this.dom.currentSourceInfo.classList.contains('hidden')){
+      var y = -this.dom.currentSourceInfo.offsetHeight;
+      this.dom.currentSourceInfo.style.transform = 'translateY(' + y + 'px)';
+      Utils.runEventOnce(this.dom.currentSourceInfo, 'transitionend', function(){
+        this.dom.currentSourceInfo.classList.add('hidden');
+      }.bind(this));
+    }
+    else {
+      this.dom.currentSourceInfo.classList.remove('hidden');
+      Utils.putOnEventQueue(function(){
+        this.dom.currentSourceInfo.style.transform = 'translateY(0px)';
+      }.bind(this));
+    }
+  },
   setInfo: function(source){
     Utils.empty(this.dom.currentSourceInfo);
     Utils.empty(this.dom.nowPlayingText);
